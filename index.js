3b6ff005425adeb222574ad87bfafca1a05a2d42
Mike Pokalsky <mpoksky1989@gmail.com>
2020-07-12 17:05:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-'use strict';
-
 function theBeatlesPlay(musicians, instruments) {
-  var array = [];
-  for (var i = 0; i < musicians.length; i++) {
-    array.push(musicians[i] + " plays " + instruments[i]);
-  }
-  return array;
+    const array = []
+      for (let i = 0, l = musicians.length; i < l; i++) {
+        array.push(`${musicians[i]} plays ${instruments[i]}`)
+      }
+      return array
 }
 
-function johnLennonFacts() {
-  var facts = [];
-  var array = [];
-  var i = 0;
-  while (array.length < facts.length) {
-    array.push(facts[i] + "!!!");
-    i++;
+function johnLennonFacts(facts) {
+  const excitedFacts = []
+
+  let i = 0;
+  while(i < facts.length) {
+    excitedFacts.push(`${facts[i]}!!!`)
+    i++
   }
-  return array;
+  return excitedFacts
 }
 
-function iLoveTheBeatles(number) {
-  var array = [];
-  do {
-    array.push("I love the Beatles!");
-    number += 1
-  } while (number < 15);
-  return array;
+function iLoveTheBeatles(n) {
+  const love = []
+
+  do{
+    love.push("I love the Beatles!")
+    n++
+  } while ( n < 15)
+  return love
 }
