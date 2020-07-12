e083108b7b95bdf0d19cba8ff571e886a0ef1e7b
Mike Pokalsky <mpoksky1989@gmail.com>
2020-07-12 16:42:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,10 @@ function johnLennonFacts() {
   }
   return array;
 }
-const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+const facts = [
+  "While on tour, he enjoyed playing Monopoly",
+  "Harmonica was the first instrument Lennon learned to play"
+];
 function iLoveTheBeatles(number) {
   var array = [];
   do {
