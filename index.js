1b1ea75b668087dc9a9a01ffc6877300f4b0728e
Mike Pokalsky <mpoksky1989@gmail.com>
2020-07-12 16:40:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function johnLennonFacts() {
   }
   return array;
 }
-var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
 function iLoveTheBeatles(number) {
   var array = [];
   do {
